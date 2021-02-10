bee8cdc3c2d039109252a62d39d6bda9f7b3349e
david dehe <david.dehe.pro@gmail.com>
2021-02-10 02:03:14 +0100

    add link between data base and API connection
    find a way to display with the flatlist the different movies
    Next commit witll be for images

--- src/pages/Search_page.js
+++ src/pages/Search_page.js
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	FlatList,
+	Image,
+} from "react-native"
 import { search_movie } from "../services/Search_Api"
 import Search from "../components/Search"
 
@@ -7,20 +14,46 @@ export default function Search_page() {
 	const [datas, set_datas] = useState([])
 	const [take_text, set_take_text] = useState("")
 
-	useEffect(() => {
-		search_movie().then((data) => {
+	const test = () => {
+		search_movie(take_text).then((data) => {
 			set_datas(data)
 		})
-	}, [])
+	}
 
 	const handleSearchText = (text) => {
 		set_take_text(text)
+		test()
 	}
-	console.log(datas)
-	console.log(take_text)
+
+	let renderItem = (item) => {
+		console.log(item)
+		return (
+			<View style={styles.cards}>
+				<Text style={styles.textCards}>{item.original_title}</Text>
+				<Text style={styles.textCards}>{item.release_date}</Text>
+				<Image
+					style={styles.logo}
+					source={{
+						url: `https://image.tmdb.org/t/p/original/1Lhc32P0a62BgMFgd20wXR1osR3.jpg`,
+						// url: `https://image.tmdb.org/t/p/original/${item.backdrop_path}`,
+					}}
+				/>
+			</View>
+		)
+	}
+
+	// console.log(datas)
+	// console.log(take_text)
 	return (
 		<View>
 			<Search handleSearch={handleSearchText} style={styles.main_container} />
+			<SafeAreaView>
+				<FlatList
+					data={datas.results}
+					renderItem={({ item }) => renderItem(item)}
+					keyExtractor={(item) => item.id.toString()}
+				/>
+			</SafeAreaView>
 		</View>
 	)
 }
@@ -30,5 +63,18 @@ const styles = StyleSheet.create({
 		width: 100,
 		height: 50,
 		backgroundColor: "red",
+	},
+	cards: {
+		width: "100%",
+		height: 40,
+		textAlign: "center",
+		backgroundColor: "red",
+	},
+	textCards: {
+		color: "black",
+	},
+	logo: {
+		width: 66,
+		height: 58,
 	},
 })
