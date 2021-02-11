41bf616c627efb8991e0ca578a07dd1a4c476fbe
david dehe <david.dehe.pro@gmail.com>
2021-02-11 17:55:22 +0100

    check antoine files

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-// import Main from "./src/pages/Main_page"
+import Main from "./src/pages/Main_page"
 import Search_page from "./src/pages/Search_page"
 import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			{/* <Main /> */}
+			<Main />
 			<Search_page style={styles.main_container} />
 		</View>
 	)
