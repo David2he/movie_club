04d6e339ebac8246ebe72f439f519c6d420f5ab7
david dehe <david.dehe.pro@gmail.com>
2021-02-11 22:54:00 +0100

    clean CategoryScreen.Js
    now put the front, good luck owel

--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react"
-import { FlatList, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from "react-native"
+import {
+	FlatList,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+} from "react-native"
 import { FindCategory } from "../services/Search_Api"
 
 export default function Main_page() {
@@ -16,12 +23,12 @@ export default function Main_page() {
 			<View style={styles.cards}>
 				<TouchableOpacity style={styles.button}>
 					<Text style={styles.textCards}>{item.name}</Text>
-      			</TouchableOpacity>
+				</TouchableOpacity>
 			</View>
 		)
 	}
 
-	console.log(datas.genres)
+	// console.log(datas.genres)
 	return (
 		<SafeAreaView style={styles.main_container}>
 			<FlatList
@@ -37,27 +44,27 @@ export default function Main_page() {
 const styles = StyleSheet.create({
 	main_container: {
 		flex: 1,
-		alignItems: 'spaceBetween',
-		width: '100%',
+		alignItems: "spaceBetween",
+		width: "100%",
 		marginTop: 20,
 	},
 	cards: {
-		width: '40%',
+		width: "40%",
 		marginTop: 24,
-		marginHorizontal: '5%',
+		marginHorizontal: "5%",
 		paddingVertical: 20,
-		textAlign: 'center',
-		backgroundColor: '#fff',
+		textAlign: "center",
+		backgroundColor: "#fff",
 		shadowColor: "#B00020",
-      	shadowOffset: {
-        width: 0,
-        height: 4,
-      },
-      shadowOpacity: 0.7,
-      shadowRadius: 4,
-      elevation: 3
+		shadowOffset: {
+			width: 0,
+			height: 4,
+		},
+		shadowOpacity: 0.7,
+		shadowRadius: 4,
+		elevation: 3,
 	},
 	textCards: {
-		color: '#B00020',
-	}
+		color: "#B00020",
+	},
 })
--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import {View, StyleSheet, Text, Image } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react"
+import {
+	View,
+	StyleSheet,
+	Text,
+	Image,
+	Button,
+	SafeAreaView,
+	FlatList,
+} from "react-native"
+import RenderItemFlatList from "../components/Render_item_flatList"
+import { Ionicons } from "@expo/vector-icons"
+import { findGenresMovies } from "../services/Search_Api"
 
 export const CategoryScreen = (props) => {
-    return (
-        <View style={styles.main_container}>
-            <Text style={styles.category_title}>Aventure</Text>
-        </View>
+	const [datas, setDatas] = useState([])
+	const setGenreSearch = "&with_grenres="
+	const genres = `${setGenreSearch}Action`
 
-    )
+	useEffect(() => {
+		findGenresMovies(genres, 1).then((data) => {
+			setDatas(data)
+		})
+	}, [])
+
+	const renderItem = (item) => {
+		console.log(item)
+		return <RenderItemFlatList test={item} />
+	}
+
+	return (
+		<View style={styles.main_container}>
+			<Text style={styles.category_title}>Aventure</Text>
+			<SafeAreaView>
+				<FlatList
+					data={datas.results}
+					renderItem={({ item }) => renderItem(item)}
+					keyExtractor={(item) => item.id.toString()}
+				/>
+			</SafeAreaView>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        backgroundColor: '#B00020',
-        paddingVertical: 20,
-    },
-    category_title: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        color: '#B5A90F',
-        fontSize: 24,
-    }
+	main_container: {
+		backgroundColor: "#B00020",
+		paddingVertical: 20,
+	},
+	category_title: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		textAlign: "center",
+		color: "#B5A90F",
+		fontSize: 24,
+	},
 })
