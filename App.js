dc915f3b0fa6a1ec041c7332e62f5851ab2f8fac
david dehe <david.dehe.pro@gmail.com>
2021-02-12 02:58:47 +0100

    Set the detailled movie page
    We just have to bring the data from the before page to this one and it will work

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-//import Search_page from "./src/pages/Search_page";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import BottomNavigator from './src/components/BottomNavigator';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import BottomNavigator from "./src/components/BottomNavigator"
 
 export default function App() {
 	return (
 		<NavigationContainer>
-      		<BottomNavigator/>
-    	</NavigationContainer>
+			<BottomNavigator />
+		</NavigationContainer>
 	)
 }
 
